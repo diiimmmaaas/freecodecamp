92d7568b3abd88669df7b9e1084cbd5bff7d317b
Dmitry Sokolov <dm.sokolov1994@gmail.com>
2022-02-23 12:41:58 +0300

    codewars

---
+++ CodeWars/35.js
@@ -0,0 +1,12 @@
+function makeNegative(num) {
+    return num > 0 ? -(num) : num
+/*
+    if (num > 0){
+        return -(num)
+    }
+    return num*/
+}
+
+console.log(makeNegative(42))
+console.log(makeNegative(0))
+console.log(makeNegative(-2))
---
+++ CodeWars/37.js
@@ -0,0 +1,7 @@
+function removeChar(str){
+    let newStr = str.slice(1, -1)
+    return newStr
+};
+
+console.log(removeChar('eloquent'))
+console.log(removeChar('country'))
